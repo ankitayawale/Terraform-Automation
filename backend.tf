2f9ecce08cef8f62936fcd2dec22545efc72e6d6
ankitayawale <111716824+ankitayawale@users.noreply.github.com>
2025-01-06 19:02:15 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform"
-    key = "jenkins-server-key"
-    region = "ap-south-1"
-    dynamodb_table = "my-dynamodb-table-25"
+    bucket = "young-minds-app-ankita"
+    key = "jenkins-server"
+    region = "us-east-1"
+    dynamodb_table = "my-dynamodb"
   }
 }

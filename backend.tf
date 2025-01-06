424c142f97527ea221f1d499eb6a181d7e916aec
ankitayawale <111716824+ankitayawale@users.noreply.github.com>
2025-01-06 18:13:30 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "mydev-project-terraform"
     key = "jenkins-server-key"
     region = "ap-south-1"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "my-dynamodb-table-25"
   }
 }

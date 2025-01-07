0ab394f32e1fee13c6b4c623bbea8283d666d125
ankitayawale <111716824+ankitayawale@users.noreply.github.com>
2025-01-07 11:14:24 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app-ankita"
+    bucket = "barclays-bucket"
     key = "jenkins-server"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb"
